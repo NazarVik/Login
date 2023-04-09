f17c21d861d8b1e5adb98b9d925e1b6bb5d8680d
NazarVik <Nazarov_dev@icloud.com>
2023-04-09 19:21:23 +0300

    home work ready

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -9,21 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
     // MARK: - outletes
-    @IBAction func mailField(_ sender: UITextField) {
-    }
-    @IBAction func passField(_ sender: UITextField) {
-    }
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signButton: UIButton!
+    @IBOutlet weak var passField: UITextField!
+    @IBOutlet weak var mailField: UITextField!
+    @IBOutlet weak var mailView: UIView!
+    @IBOutlet weak var passView: UIView!
     
-    @IBAction func loginButton(_ sender: UIButton) {
-    }
-    @IBAction func signButton(_ sender: Any) {
-    }
-    
+    //MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loginButton.layer.shadowOpacity = 0.4
+        loginButton.layer.shadowOffset = CGSize(width: 0, height: 3)
+        loginButton.layer.shadowRadius = 3
+        loginButton.layer.shadowColor = UIColor.systemPink.cgColor
     }
 
-
+    //MARK: - Actions
+    @IBAction func loginAction(_ sender: Any) {
+    }
+    @IBAction func signAction(_ sender: Any) {
+    }
 }
 

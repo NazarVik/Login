f98958cc55b1b9726e079a7d5a9bb0fed0a9671d
NazarVik <Nazarov_dev@icloud.com>
2023-04-09 15:18:55 +0300

    lesson work

---
+++ Login/Login/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  Login
+//
+//  Created by Виктор Назаров on 9.04.23.
+//
+
+import Foundation
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+}
---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Виктор Назаров on 8.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - outletes
+    @IBAction func mailField(_ sender: UITextField) {
+    }
+    @IBAction func passField(_ sender: UITextField) {
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    @IBAction func signButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
